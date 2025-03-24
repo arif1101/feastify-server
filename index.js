2af108bef1e8438d2c861d9dd531a6f24cf237b4
arif1101 <arifurrahmanarif223@gmail.com>
2025-03-25 03:06:18 +0600

    delete user by id

--- index.js
+++ index.js
@@ -49,7 +49,15 @@ async function run() {
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
-    // user API end 
+
+    app.delete('/users/id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
+    
+    // end user API 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
