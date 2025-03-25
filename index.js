42dd9c648a6afedbd5c5dff422b0ee724ac5816c
arif1101 <arifurrahmanarif223@gmail.com>
2025-03-26 01:51:45 +0600

    patch to give user role

--- index.js
+++ index.js
@@ -50,10 +50,22 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/users/id', async(req,res) => {
+    app.delete('/users/:id', async(req,res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc)
       res.send(result)
     })
     
