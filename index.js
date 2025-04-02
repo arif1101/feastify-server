33fcc96ff9f2b1baca019af900ede3c56087d4a2
arif1101 <arifurrahmanarif223@gmail.com>
2025-04-03 05:04:55 +0600

    verifyAdmin

--- index.js
+++ index.js
@@ -61,10 +61,21 @@ async function run() {
           req.decoded = decoded;
           next();
       });
-  };
+    };
+
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      const isAdmin =user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next()
+    }
 
     // user API 
-    app.get('/users',verifyToken , async(req,res) => {
+    app.get('/users',verifyToken, verifyAdmin, async(req,res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
