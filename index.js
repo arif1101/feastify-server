dcbf2ad24ce0b9edd08bd2de8baf4d6ad4036d86
arif1101 <arifurrahmanarif223@gmail.com>
2025-04-03 01:05:06 +0600

    verify token

--- index.js
+++ index.js
@@ -41,11 +41,26 @@ async function run() {
       res.send({token})
     })
 
+    const verifyToken = (req,res,next) => {
+      console.log('inside verify token', req.headers)
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: "forbidden access"})
+        }
+        req.decoded=decoded;
+        next();
+      })
+    }
+
     // user API 
-    app.get('/users', async(req,res) => {
+    app.get('/users',verifyToken , async(req,res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    }); 
+    });
     
     app.post('/users', async(req,res)=> {
       const user = req.body;
