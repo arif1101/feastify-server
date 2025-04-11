602bd2b661b7dd0b9d48b2c50862a2957bedbb58
arif1101 <arifurrahmanarif223@gmail.com>
2025-04-11 18:02:09 +0600

    fixed payment to payments

--- index.js
+++ index.js
@@ -205,12 +205,13 @@ async function run() {
 
     // payment
 
-    app.get('/payment/:email', verifyToken, async (req, res) => {
+    app.get('/payments/:email', verifyToken, async (req, res) => {
       const query = {email: req.params.email}
       if(req.params.email !== req.decoded.email){
         return res.status(403).send({message: 'forbidden access'})
       }
       const result = await paymentCollection.find(query).toArray();
+      console.log(result)
       res.send(result);
     })
     
